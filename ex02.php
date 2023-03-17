97c67e33c94ef98f7d2c1d9bd887fcf345a513ac
Mateus Ribeiro <128062321+mateuzo0@users.noreply.github.com>
2023-03-17 09:34:52 -0300

    Add files via upload
    tinha esquecido de adicionar esse

---
+++ ex02.php
@@ -0,0 +1,26 @@
+<?php
+    $n1 = $_POST["n1"];
+    $n2 = $_POST["n2"];
+
+    if ($soma = $n1 + $n2){
+        echo "A soma de $n1 e $n2 é igual a $soma<br>";
+    }
+    if ($subtracao = $n1 - $n2){
+        echo "A subtração de $n1 e $n2 é igual a $subtracao<br>";
+    }
+    if ($divisao = $n1 / $n2){
+        echo "A divisão de $n1 e $n2 é igual a $divisao<br>";
+    }
+    if ($multiplicacao = $n1 * $n2){
+        echo "A multiplicação de $n1 e $n2 é igual a $multiplicacao<br>";
+    }
+    if ($potenciacao = $n1 ** $n2){
+        echo "A potenciação de $n1 a potencia de $n2 é igual a $potenciacao<br>";
+    }
+    if ($raizq = sqrt($n1) ){
+        echo "A raiz quadrado de $n1 é igual a $raizq<br>";
+    }
+    if ($raizq = sqrt($n2) ){
+        echo "A raiz quadrado de $n2 é igual a $raizq<br>";
+    }
+?>
